8124628f8580cce4bf4306cd77e3be05f5268c6c
Floral <49110090+floral-qua-floral@users.noreply.github.com>
2025-05-22 22:34:10 -0400

    Moved some external assets around. Mario now removes his cap when wearing a helmet and doesn't have it at all when in Small form. Fixed Small Mario model's chestplate right arm bottom face UV.
    Took 2 hours 32 minutes

--- content/src/main/java/com/fqf/mario_qua_mario_content/util/Powers.java
+++ content/src/main/java/com/fqf/mario_qua_mario_content/util/Powers.java
@@ -4,7 +4,7 @@ public class Powers {
 	public static final String DROP_COINS = "Drop coins";
 	public static final String LIGHTNING_SHRINKS = "Lightning causes Mini form";
 	public static final String CEILING_CLIPPING = "Clips past ceiling edges on ascent";
-	public static final String STOMP_GUARD = "Brief melee immunity after stomping";
+	public static final String STOMP_GUARD = "Protected from underfoot melee attacks";
 
 	public static final String SLEEPY = "Falls asleep while idle";
 
