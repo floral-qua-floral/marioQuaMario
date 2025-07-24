634e7a0ad98c5d063b36113d2c0221f4b08248ae
Floral <49110090+floral-qua-floral@users.noreply.github.com>
2025-07-23 21:59:24 -0400

    Refined tests, ported in current implementation from Bappable interface
    Took 1 hour 12 minutes

--- bapcalculator/Bap Calculator/BapCalculator.py
+++ bapcalculator/Bap Calculator/BapCalculator.py
@@ -1,13 +1,33 @@
-def get_bap_result(hardness, force):
-    return NO_SELL
+ELEPHANT_MARIO_COLLISION_STRENGTH = 5
+SUPER_MARIO_COLLISION_STRENGTH = 4
+SMALL_MARIO_COLLISION_STRENGTH = 3
 
+def get_bap_result(hardness, blast_resistance, strength):
+    if hardness < strength * 0.1:
+        return THRU
+
+    if hardness < strength * 0.125:
+        return BREAK
+
+    if hardness < strength * 0.375:
+        return CRACK
+
+    if strength <= 2:
+        bump_threshold = 0
+    elif strength == 3:
+        bump_threshold = 3
+    else:
+        bump_threshold = 1 + strength * 0.5
+
+    if hardness < bump_threshold:
+        return BUMP
+
+    return NO_SELL
 
 NO_SELL, BUMP, CRACK, BREAK, THRU = range(5)
 bapResultNames = ["NO-SELL", "BUMP", "CRACK", "BREAK", "GO-THRU"]
 
 testsRun, testsPassed = 0, 0
-
-
 def test_bap(test_name, expected_result, got_result):
     global testsRun, testsPassed
     testsRun += 1
@@ -23,43 +43,43 @@ def test_bap(test_name, expected_result, got_result):
             testsPassed += 1
         else:
             print(
-                f"* Test {test_name} FAILED ;-;\n\tGot {bapResultNames[got_result]}, expected one of ({acceptable_results})\n")
+                f"* Test {test_name} FAILED\n\tGot {bapResultNames[got_result]}, expected one of ({acceptable_results})\n")
     else:
         if expected_result == got_result:
             print(f"Test {test_name} PASSED (Got expected {bapResultNames[got_result]})\n")
             testsPassed += 1
         else:
             print(
-                f"* Test {test_name} FAILED ;-;\n\tGot {bapResultNames[got_result]}, expected {bapResultNames[expected_result]}\n")
+                f"* Test {test_name} FAILED\n\tGot {bapResultNames[got_result]}, expected {bapResultNames[expected_result]}\n")
 
 
 if __name__ == "__main__":
     print("Testing the bap calculator:")
 
-    test_bap("Super Pound on Moss Block", THRU, get_bap_result(0.1, 4))
-    test_bap("Small Pound on Moss Block", (BREAK, THRU), get_bap_result(0.1, 3))
+    test_bap("Super Pound on Moss Block", THRU, get_bap_result(0.1, 0.1, SUPER_MARIO_COLLISION_STRENGTH))
+    test_bap("Small Pound on Moss Block", (BREAK, THRU), get_bap_result(0.1, 0.1, 3))
 
-    test_bap("Super Pound on Leaves", (BREAK, THRU), get_bap_result(0.2, 4))  # Always passes
-    test_bap("Small Pound on Leaves", BREAK, get_bap_result(0.2, 3))
+    test_bap("Super Pound on Leaves", (BREAK, THRU), get_bap_result(0.2, 0.2, SUPER_MARIO_COLLISION_STRENGTH))  # Always passes
+    test_bap("Small Pound on Leaves", BREAK, get_bap_result(0.2, 0.2, SMALL_MARIO_COLLISION_STRENGTH))
 
-    test_bap("Super Pound on Netherrack", BREAK, get_bap_result(0.4, 4))
-    test_bap("Small Pound on Netherrack", (BREAK, CRACK), get_bap_result(0.4, 3))
+    test_bap("Elephant Pound on Netherrack", THRU, get_bap_result(0.4, 0.4, ELEPHANT_MARIO_COLLISION_STRENGTH))
+    test_bap("Super Pound on Netherrack", BREAK, get_bap_result(0.4, 0.4, SUPER_MARIO_COLLISION_STRENGTH))
+    test_bap("Small Pound on Netherrack", CRACK, get_bap_result(0.4, 0.4, SMALL_MARIO_COLLISION_STRENGTH))
 
-    test_bap("Super Pound on Grass Block", CRACK, get_bap_result(0.6, 4))
-    test_bap("Small Pound on Grass Block", BUMP, get_bap_result(0.6, 3))
+    test_bap("Super Pound on Dirt", BREAK, get_bap_result(0.5, 0.5, SUPER_MARIO_COLLISION_STRENGTH))
+    test_bap("Small Pound on Dirt", (CRACK, BUMP), get_bap_result(0.5, 0.5, SMALL_MARIO_COLLISION_STRENGTH))
 
-    test_bap("Super pound on Stone", (CRACK, BUMP), get_bap_result(1.5, 4))
-    test_bap("Small pound on Stone", (BUMP, NO_SELL), get_bap_result(1.5, 3))
+    test_bap("Super Pound on Grass Block", CRACK, get_bap_result(0.6, 0.6, SUPER_MARIO_COLLISION_STRENGTH))
+    test_bap("Small Pound on Grass Block", BUMP, get_bap_result(0.6, 0.6, SMALL_MARIO_COLLISION_STRENGTH))
 
-    test_bap("Super Pound on Wooden Planks", BUMP, get_bap_result(2, 4))
-    test_bap("Small Pound on Wooden Planks", BUMP, get_bap_result(2, 3))
+    super_pound_stone = get_bap_result(1.5, 6, SUPER_MARIO_COLLISION_STRENGTH)
+    test_bap("Super pound on Stone", (CRACK, BUMP), super_pound_stone)
+    # Whatever Super Mario does to Stone, Small Mario should be 1 tier weaker
+    test_bap("Small pound on Stone", max(0, super_pound_stone - 1), get_bap_result(1.5, 6, SMALL_MARIO_COLLISION_STRENGTH))
 
-    test_bap("Super Pound on Chest", BUMP, get_bap_result(2.5, 4))
-    test_bap("Small Pound on Chest", (BUMP, NO_SELL), get_bap_result(2.5, 3))
+    test_bap("Super Pound on Wooden Planks", BUMP, get_bap_result(2, 3, SUPER_MARIO_COLLISION_STRENGTH))
+    test_bap("Small Pound on Wooden Planks", BUMP, get_bap_result(2, 3, SMALL_MARIO_COLLISION_STRENGTH))
 
-    test_bap("Super Pound on End Stone, Copper, Doors, Gold", (BUMP, NO_SELL), get_bap_result(3, 4))
-    test_bap("Small Pound on End Stone, Copper, Doors, Gold", (BUMP, NO_SELL), get_bap_result(3, 3))
-
-    test_bap("Super Pound on Iron Block", NO_SELL, get_bap_result(5, 4))
+    test_bap("Super Pound on Iron Block", NO_SELL, get_bap_result(5, 6, SUPER_MARIO_COLLISION_STRENGTH))
 
     print(f"FINAL RESULTS:\n\t{testsPassed}/{testsRun} passed ({round(testsPassed / testsRun * 100, 1)}%)\n\t{testsRun - testsPassed} failed")
