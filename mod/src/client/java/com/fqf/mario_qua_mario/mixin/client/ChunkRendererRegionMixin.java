05b2231df6fb512715d80636bcaf945e16e9e4cc
Floral <49110090+floral-qua-floral@users.noreply.github.com>
2025-07-31 20:09:34 -0400

    Fixed blocks not being hidden when Sodium isn't installed
    Took 7 minutes

--- mod/src/client/java/com/fqf/mario_qua_mario/mixin/client/ChunkRendererRegionMixin.java
+++ mod/src/client/java/com/fqf/mario_qua_mario/mixin/client/ChunkRendererRegionMixin.java
@@ -2,6 +2,7 @@ package com.fqf.mario_qua_mario.mixin.client;
 
 import com.fqf.mario_qua_mario.bapping.BlockBappingUtil;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -16,8 +17,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ChunkRendererRegionMixin {
 	@Shadow @Final protected World world;
 
-	@Inject(method = "getBlockState", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "getBlockState", at = @At("RETURN"), cancellable = true)
 	private void hideHiddenBlocks(BlockPos pos, CallbackInfoReturnable<BlockState> cir) {
+//		cir.setReturnValue(Blocks.AIR.getDefaultState());
 		BlockBappingUtil.conditionallyHideBlockPos(this.world, pos, cir);
 	}
 }
