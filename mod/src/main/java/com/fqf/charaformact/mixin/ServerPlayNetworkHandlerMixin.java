2ffb00533e24136917de8cd582ca988927a873bf
Floral <49110090+floral-qua-floral@users.noreply.github.com>
2026-08-27 04:09:28 -0400

    Fixed an unbearably infuriating miserable awful "moved wrongly" bug that's been torturing me for literal years and I'm pretty sure it was all vanilla's fault
    Took 2 hours 4 minutes

--- mod/src/main/java/com/fqf/charaformact/mixin/ServerPlayNetworkHandlerMixin.java
+++ mod/src/main/java/com/fqf/charaformact/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,20 +1,18 @@
 package com.fqf.charaformact.mixin;
 
-import com.fqf.charaformact.registries.actions.UniversalActionDefinitionHelper;
 import com.fqf.charaformact.util.CfaGamerules;
 import com.fqf.charaformact.util.CfaPositionSettable;
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.network.packet.s2c.play.PositionFlag;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.Set;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin implements CfaPositionSettable {
@@ -24,25 +22,43 @@ public class ServerPlayNetworkHandlerMixin implements CfaPositionSettable {
 
 	@Shadow private double updatedX, updatedY, updatedZ;
 
-	@WrapOperation(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isInTeleportationState()Z"))
+	@WrapOperation(
+			method = "onPlayerMove",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/server/network/ServerPlayerEntity;isInTeleportationState()Z"
+			)
+	)
 	private boolean isTeleportingOrCharacter(ServerPlayerEntity instance, Operation<Boolean> original) {
 		return (instance.getServerWorld().getGameRules().getBoolean(CfaGamerules.DISABLE_CHARACTER_MOVEMENT_CHECK) && instance.cfa$getCfaData().isEnabled()) || original.call(instance);
 	}
 
-//	@Inject(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V"))
-//	private void uwu(PlayerMoveC2SPacket packet, CallbackInfo ci) {
-//		CharaFormAct.LOGGER.info("Moved wrongly. Information:\n\tLast tick: ({}, {}, {})\n\tPlayer position: ({}, {}, {})\n\tPacket target: ({}, {}, {})",
-//				this.lastTickX, this.lastTickY, this.lastTickZ,
-//				player.getX(), player.getY(), player.getZ(),
-//				packet.getX(0), packet.getY(0), packet.getZ(0));
-//	}
-
-
-//	@WrapMethod(method = "requestTeleport(DDDFFLjava/util/Set;)V")
-//	private void requestTeleportHook(double x, double y, double z, float yaw, float pitch, Set<PositionFlag> flags, Operation<Void> original) {
-//		if(!this.player.cfa$getCfaData().skipDismountRelatedTeleports)
-//			original.call(x, y, z, yaw, pitch, flags);
-//	}
+	@Unique private boolean lastTickWasOnGround;
+	@Inject(
+			method = "syncWithPlayerPosition",
+			at = @At("HEAD")
+	)
+	private void storeLastTickWasOnGround(CallbackInfo ci) {
+		this.lastTickWasOnGround = this.player.isOnGround();
+	}
+	@Inject(
+			method = "tick",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/server/network/ServerPlayerEntity;updatePositionAndAngles(DDDFF)V",
+					shift = At.Shift.AFTER
+			)
+	)
+	private void restoreOnGroundStatusThatActuallyMatchesThePositionWeJustSetOhMyGod(CallbackInfo ci) {
+		// This can update the player's position to be in midair, without resetting their isOnGround status.
+		// As a result, it can cause adjustMovementForSneaking to fire even while the player isn't actually close
+		// enough to the ground to detect it, which causes adjustMovementForSneaking to completely zero out the input
+		// motion.
+		// I think the only reason this doesn't cause "moved wrongly" errors in vanilla is that it requires the player
+		// to be falling relatively fast while also sneaking while also traveling horizontally at a high enough speed
+		// for the discrepancy to trip the "moved wrongly" check.
+		this.player.setOnGround(this.lastTickWasOnGround);
+	}
 
 	@Override
 	public void cfa$setPos(Vec3d pos) {
